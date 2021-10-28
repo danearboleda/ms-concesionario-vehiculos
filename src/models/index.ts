75bdc36854a8046457d771f55a38aba69497c9fb
danearboleda <nataarboleda2001@gmail.com>
2021-10-28 17:57:30 -0500

    Corrección de inserción extra

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
-export * from './marca.model';
-export * from './categoria.model';
-export * from './proveedor.model';
-export * from './foto.model';
-export * from './vehiculo.model';
 export * from './categoria-vehiculo.model';
+export * from './categoria.model';
+export * from './foto.model';
+export * from './marca.model';
+export * from './proveedor.model';
+export * from './vehiculo.model';
+
